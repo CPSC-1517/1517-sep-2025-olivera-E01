9078a246d804d4c0f5e4c942db4fbd855400ca1d
Oliver Antoniu <oliver+github@abolish.golf>
2025-09-29 19:56:42 -0600

    add [02 - intro to testing]: HiringHelper.Tests - FullName test

---
+++ 02 - intro to testing/02a - HiringHelper/HiringHelper.Tests/Employment_validation.cs
@@ -0,0 +1,20 @@
+﻿namespace HiringHelper
+{
+
+    using FluentAssertions;
+    using Xunit;
+
+    public class Employment_validation
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")] // fixme: does not throw exception
+        public void FullName_Blank_Throws(string? bad)
+        {
+            var act = () => new Employment { FullName = bad };
+            act.Should().Throw<ArgumentException>()
+                .WithMessage("*FullName*");
+        }
+    }
+}
