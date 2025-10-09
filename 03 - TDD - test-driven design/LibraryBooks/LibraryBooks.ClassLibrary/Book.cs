b8a46b2ec9b153f4de9345187b09ecbd40bc29a9
Oliver Antoniu <oliver+github@abolish.golf>
2025-10-08 21:31:36 -0600

    rev [03 - TDD]: LibraryBooks example, Book fully implemented

--- 03 - TDD - test-driven design/LibraryBooks/LibraryBooks.ClassLibrary/Book.cs
+++ 03 - TDD - test-driven design/LibraryBooks/LibraryBooks.ClassLibrary/Book.cs
@@ -24,21 +24,61 @@ namespace LibraryBooks.ClassLibrary
         public string ISBN
         {
             get => _isbn;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(ISBN), "ISBN is required (non-empty, non-blank).");
+                }
+
+                _isbn = value.Trim();
+            }
+
         }
 
         public string Title
         {
             get => _title;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(Title), "Title is required (non-empty, non-blank).");
+                }
+
+                _title = value.Trim();
+            }
         }
 
         public string Author
         {
             get => _author;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(Author), "Author is required (non-empty, non-blank).");
+                }
+
+                _author = value.Trim();
+            }
         }
 
         public int Pages
         {
             get => _pages;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Pages must be a positive non-zero number. Received: {value}.", nameof(Pages));
+                }
+
+                if (value < MIN_PAGES)
+                {
+                    throw new ArgumentException($"Pages must be at least {MIN_PAGES}. Received {value}.", nameof(Pages));
+                }
+            }
         }
 
         public Book (string isbn, string title, string author, int pages)
@@ -56,7 +96,5 @@ namespace LibraryBooks.ClassLibrary
         public override string ToString() => $"{ISBN} - {Title} - {Author}, {Pages}pp.";
 
 
-        //   - should have the following behaviour:
-        //       - no data fields above should be externally mutable (modifiable)
     }
 }
