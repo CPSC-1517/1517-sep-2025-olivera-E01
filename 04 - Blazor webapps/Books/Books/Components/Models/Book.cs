82a8ecb301aab04c2045e16410bcf9895a399299
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-05 19:49:49 -0700

    rev [04 - Blazor]: Books webapp - form validation, minimal implementation

--- 04 - Blazor webapps/Books/Books/Components/Models/Book.cs
+++ 04 - Blazor webapps/Books/Books/Components/Models/Book.cs
@@ -1,7 +1,17 @@
-﻿namespace Books.Models
+﻿// this is the built-in namespace/collection of data validators
+using System.ComponentModel.DataAnnotations;
+
+// to use them, we can look at all the options in the docs:
+//    https://learn.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations?view=net-9.0
+//
+//    and attach them to a property by calling any in this list ^
+//    without the "Attribute" part at the end
+
+namespace Books.Models
 {
     public class Book
     {
+        [Required]  // <-- here's the validator, attached to Title; we're using RequiredAttribute
         public string? Title { get; set; }
         public int Pages { get; set; }
         public DateOnly PublishDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
