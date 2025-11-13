f94aacd06c87b438c963de7692ec904705286792
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-12 20:10:52 -0700

    add [04 - Blazor]: Books webapp - DIY form validation without DataAnnotations (using editContext & messageStore)

---
+++ 04 - Blazor webapps/Books/Books/Components/Models/BookWithoutDataAnnotations.cs
@@ -0,0 +1,17 @@
+﻿namespace Books.Models
+{
+    public class BookWithoutDataAnnotations
+    {
+        public string? Title { get; set; }
+
+        public int Pages { get; set; }
+        public DateOnly PublishDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public string Genre { get; set; } = "Fiction";
+        public bool InStock { get; set; }
+
+        public string ToCsv()
+        {
+            return $"{Title},{Pages},{PublishDate},{Genre},{InStock}\n";
+        }
+    }
+}
