35a527985c012b88e2ead7c15983753922c30dcb
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-12 21:07:27 -0700

    rev [04 - Blazor]: Books webapp - add constructors to BookWithoutDataAnnotations so we can create new instances from parsed CSV strings

--- 04 - Blazor webapps/Books/Books/Components/Models/BookWithoutDataAnnotations.cs
+++ 04 - Blazor webapps/Books/Books/Components/Models/BookWithoutDataAnnotations.cs
@@ -9,6 +9,18 @@
         public string Genre { get; set; } = "Fiction";
         public bool InStock { get; set; }
 
+        // we need some instance constructors if we're going to create instances from a parsed CSV string
+        public BookWithoutDataAnnotations() { }
+
+        public BookWithoutDataAnnotations(string title, int pages, DateOnly publishDate, string genre, bool inStock)
+        {
+            Title = title;
+            Pages = pages;
+            PublishDate = publishDate;
+            Genre = genre;
+            InStock = inStock;
+        }
+
         public string ToCsv()
         {
             return $"{Title},{Pages},{PublishDate},{Genre},{InStock}\n";
