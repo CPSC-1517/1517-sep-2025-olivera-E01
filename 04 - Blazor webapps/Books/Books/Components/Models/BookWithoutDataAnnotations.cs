8176b779f3b64a33ef91dd6c2a61b9fa6c3b090a
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-12 21:17:44 -0700

    rev [04 - Blazor]: Books webapp - write logic to parse CSV line into new instance with some basic error handling

--- 04 - Blazor webapps/Books/Books/Components/Models/BookWithoutDataAnnotations.cs
+++ 04 - Blazor webapps/Books/Books/Components/Models/BookWithoutDataAnnotations.cs
@@ -25,5 +25,34 @@
         {
             return $"{Title},{Pages},{PublishDate},{Genre},{InStock}\n";
         }
+
+        public static BookWithoutDataAnnotations ParseFromCsv(string csvLine)
+        {
+            if (string.IsNullOrWhiteSpace(csvLine))
+            {
+                throw new ArgumentNullException(nameof(csvLine), "Line cannot be blank.");
+            }
+
+            // shape of data: "title,pages,publishDate,genre,inStock"
+            //                 0     1     2           3     4
+            // we're going to split by comma into a list; ^these^ will be each property's
+            // numerical index in that list
+            string[] fields = csvLine.Split(",");  // "split the string by commas into a list of elements"
+
+            if (fields.Length != 5)
+            {
+                throw new FormatException($"Incorrect number of input terms in:\n{csvLine}");
+            }
+
+            // naive implementation: cast each array element into a variable
+            string title = fields[0];
+            int pages = int.Parse(fields[1]);
+            DateOnly publishDate = DateOnly.Parse(fields[2]);
+            string genre = fields[3];
+            bool inStock = bool.Parse(fields[4]);
+
+            // finally, use the greedy constructor to return an instance hydrated with the above values
+            return new BookWithoutDataAnnotations(title, pages, publishDate, genre, inStock);
+        }
     }
 }
