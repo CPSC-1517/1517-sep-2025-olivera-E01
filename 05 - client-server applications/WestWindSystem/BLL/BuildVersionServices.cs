1ce3c41ae2a1cf90358250dae7fbf0ce154a8af6
Oliver Antoniu <oliver+github@abolish.golf>
2025-11-26 20:15:41 -0700

    pkg [10 - client-server apps]: WestWind app - install BlazorBootstrap via NuGet

--- 05 - client-server applications/WestWindSystem/BLL/BuildVersionServices.cs
+++ 05 - client-server applications/WestWindSystem/BLL/BuildVersionServices.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WestWindSystem.DAL;
-using WestWindSystem.Entities;
 
 #region Additional Namespaces
 using WestWindSystem.DAL;
